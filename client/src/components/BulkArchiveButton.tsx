62adee11eab8fa9224b8fc2a1bcc251d7205b865
axgu <annaxigu@gmail.com>
2026-03-17 15:48:53 -0400

    change bulk archive feature to 60 days

--- client/src/components/BulkArchiveButton.tsx
+++ client/src/components/BulkArchiveButton.tsx
@@ -61,13 +61,13 @@ export default function BulkArchiveButton(props: { fetchItems: () => void }) {
           fontSize: "18px",
         }}
       >
-        <p>Are you sure you wish to archive all items older than 90 days?</p>
+        <p>Are you sure you wish to archive all items older than 60 days?</p>
         <Button
           onClick={() => {
             axios
               .post("/api/items/archiveByDays", {
                 token: localStorage.getItem("lnf_token"),
-                days: 90,
+                days: 60,
               })
               .then(
                 (res) => {
